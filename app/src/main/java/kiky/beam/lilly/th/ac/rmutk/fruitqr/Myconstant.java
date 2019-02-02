58d451603dcfaa8728ac66172329d86a66d974b3
masterUNG <phrombutr@gmail.com>
2019-02-02 14:41:53 +0700

    Detail OK

--- app/src/main/java/kiky/beam/lilly/th/ac/rmutk/fruitqr/Myconstant.java
+++ app/src/main/java/kiky/beam/lilly/th/ac/rmutk/fruitqr/Myconstant.java
@@ -5,6 +5,11 @@ public class Myconstant {
     private String urlAddUser = "https://www.androidthai.in.th/rmutk/addDataLilly.php";
     private String urlGetAllData = "https://www.androidthai.in.th/rmutk/getAllDatalilly.php";
     private String urlGetDataWhereQR = "https://www.androidthai.in.th/rmutk/getDetailWhereQRmaster.php";
+    private String urlGetUserWhereId = "https://www.androidthai.in.th/rmutk/getUserWhereId.php";
+
+    public String getUrlGetUserWhereId() {
+        return urlGetUserWhereId;
+    }
 
     public String getUrlGetDataWhereQR() {
         return urlGetDataWhereQR;
